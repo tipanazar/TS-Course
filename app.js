2c76999e609d60d81b919f51bb51ca4928971575
ololosh-tralevich <karpeko76@gmail.com>
2022-07-13 00:20:20 +0100

    Done

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function testFunc() {
     })(Status || (Status = {}));
     var union;
     var literal;
+    // = = = = = = = = = = = = = = = = = = =
     age = 50;
     name = "Max";
     toggle = true;
@@ -31,5 +32,29 @@ function testFunc() {
     union = age;
     literal = "enable";
     literal = "disable";
+    function showMessage(message) {
+        console.log(message);
+    }
+    function calc(num1, num2) {
+        return num1 + num2;
+    }
+    function customErr() {
+        throw new Error("Error");
+    }
+    var page1 = {
+        title: "The awesome page",
+        likes: 100,
+        accounts: ["Max", "Anton", "Nikita"],
+        status: "open",
+        details: {
+            createAt: "2021-01-01",
+            updateAt: "2021-05-01"
+        }
+    };
+    var page2 = {
+        title: "Python or Js",
+        likes: 5,
+        accounts: ["Alex"],
+        status: "close"
+    };
 }
-testFunc();
--- app.ts
+++ app.ts
@@ -4,9 +4,9 @@ function testFunc() {
   let toggle: boolean;
   let empty: null;
   let notInitialize = undefined;
-  let callback: (a: any) => void;
+  let callback: (a: number) => number;
   let anything: any;
-  let some: string;
+  let some: unknown;
   let person: [string, number];
   enum Status {
     LOADING,
@@ -14,6 +14,18 @@ function testFunc() {
   }
   let union: string | number;
   let literal: "enable" | "disable";
+  type DataType = {
+    title: string;
+    likes: number;
+    accounts: string[];
+    status: "open" | "close";
+    details?: {
+      createAt: string;
+      updateAt: string;
+    };
+  };
+
+  // = = = = = = = = = = = = = = = = = = =
 
   age = 50;
   name = "Max";
@@ -24,14 +36,39 @@ function testFunc() {
     return 100 + a;
   };
   anything = null;
+  anything = -20;
+  anything = "Text";
+  anything = {};
   some = "text";
-  name = some;
   person = ["Max", 21];
   union = "string";
   union = age;
   literal = "enable";
   literal = "disable";
+  function showMessage(message: string): void {
+    console.log(message);
+  }
+  function calc(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+  function customErr(): never {
+    throw new Error("Error");
+  }
 
+  const page1: DataType = {
+    title: "The awesome page",
+    likes: 100,
+    accounts: ["Max", "Anton", "Nikita"],
+    status: "open",
+    details: {
+      createAt: "2021-01-01",
+      updateAt: "2021-05-01",
+    },
+  };
+  const page2: DataType = {
+    title: "Python or Js",
+    likes: 5,
+    accounts: ["Alex"],
+    status: "close",
+  };
 }
-
-testFunc();
